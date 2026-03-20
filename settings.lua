80d9e3aaf961aca89eeb769723f43de8a4a0f2bc
triplecis <francisbou20@gmail.com>
2026-03-20 16:09:15 -0400

    uhhh

--- settings.lua
+++ settings.lua
@@ -1,16 +1,7 @@
 print('Settings module loaded')
 
---[[ Setup ]]--
-_Linoria.ThemeManager:SetLibrary(_Linoria.Library)
-_Linoria.SaveManager:SetLibrary(_Linoria.Library)
-_Linoria.SaveManager:IgnoreThemeSettings()
-_Linoria.ThemeManager:SetFolder('SMILE/themes')
-_Linoria.SaveManager:SetFolder('SMILE/configs')
-
 --[[ Groupboxes ]]--
 local SettingsMenu    = _Tabs.Settings:AddLeftGroupbox('Menu')
-local SettingsThemes  = _Tabs.Settings:AddRightGroupbox('Themes')
-local SettingsConfigs = _Tabs.Settings:AddRightGroupbox('Configs')
 local SettingsDiscord = _Tabs.Settings:AddLeftGroupbox('Discord')
 
 --[[ Menu ]]--
@@ -18,20 +9,26 @@ SettingsMenu:AddLabel("Menu keybind"):AddKeyPicker("MenuKeybind", {
     Default = "RightControl",
     NoUI = true,
     Text = "Toggle UI",
-    Callback = function()
-        _Linoria.Library.ToggleKeybind = Options.MenuKeybind.Value
-    end
 })
+
+_Linoria.Library.ToggleKeybind = Options.MenuKeybind
 
 SettingsMenu:AddButton('Close UI', function()
     _Linoria.Library:Unload()
 end)
 
---[[ Themes and Configs ]]--
-_Linoria.ThemeManager:BuildThemeSection(SettingsThemes)
-_Linoria.SaveManager:BuildConfigSection(SettingsConfigs)
-
 --[[ Discord ]]--
 SettingsDiscord:AddButton("Copy Discord", function()
     setclipboard("discord.gg/yPeD8tx2Vq")
 end)
+
+--[[ Setup Managers ]]--
+_Linoria.ThemeManager:SetLibrary(_Linoria.Library)
+_Linoria.SaveManager:SetLibrary(_Linoria.Library)
+_Linoria.SaveManager:IgnoreThemeSettings()
+_Linoria.ThemeManager:SetFolder('SMILE/themes')
+_Linoria.SaveManager:SetFolder('SMILE/configs')
+
+--[[ Themes and Configs ]]--
+_Linoria.ThemeManager:BuildThemeSection(_Tabs.Settings:AddRightGroupbox('Themes'))
+_Linoria.SaveManager:BuildConfigSection(_Tabs.Settings:AddRightGroupbox('Configs'))
